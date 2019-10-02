b2308aa8195cef0be867de859166dd6aa585d4b1
Rebecca Haggard <beccahagg5@gmail.com>
2019-10-02 12:18:18 -0600

    parses hashtags, user mentions, and urls; url link works

--- src/app/status/message/message.component.ts
+++ src/app/status/message/message.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from '../../user/user.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-message',
@@ -9,9 +10,11 @@ import {UserService} from '../../user/user.service';
 export class MessageComponent implements OnInit {
   @Input() messageText: string;
   private userService: UserService;
+  private router: Router;
 
-  constructor(userService: UserService) {
+  constructor(userService: UserService, router: Router) {
     this.userService = userService;
+    this.router = router;
   }
 
   ngOnInit() {
@@ -19,21 +22,44 @@ export class MessageComponent implements OnInit {
   }
 
   parseMessage() {
-    const regex = /#[^\s]*(?=$|\s)/g;
-    const hashtags = this.messageText.match(regex);
+    const hashtagRegex = /#[^\s]*(?=$|\s)/g;
+    const hashtags = this.messageText.match(hashtagRegex);
+    let messageHTML = this.messageText;
     if (hashtags !== null) {
-      let messageHTML = this.messageText;
-      messageHTML = messageHTML.replace(regex, (text) => {
+      messageHTML = messageHTML.replace(hashtagRegex, (text) => {
         // cut off hashtag
         text = text.substr(1);
         const link = '\"/search/' + text + '\"';
         return '<a routerLink=' + link + ' ng-reflect-router-link=' + link + ' href=' + link + '>#' + text + '</a>';
       });
-      // <a _ngcontent-kqs-c6="" id="link" ng-reflect-router-link="/search/#" href="/search/%23">#</a>
-      document.getElementById('messageText').innerHTML = messageHTML;
-      console.log(document.getElementById('messageText'));
-      console.log(document.getElementById('link'));
     }
+    const urlRegex = /http([^\s])*/g;
+    const urls = this.messageText.match(urlRegex);
+    if (urls != null) {
+      messageHTML = messageHTML.replace(urlRegex, (text) => {
+        const link = '\"' + text + '\"';
+        return '<a href=' + link + '>' + text + '</a>';
+      });
+    }
+    const userMentionsRegex = /@([^\s])*/g;
+    const userMentions = this.messageText.match(userMentionsRegex);
+    if (userMentions != null) {
+      messageHTML = messageHTML.replace(userMentionsRegex, (text) => {
+        // cut off @
+        text = text.substr(1);
+        const link = '\"/search/' + text + '\"';
+        return '<a routerLink=' + link + ' ng-reflect-router-link=' + link + ' href=' + link + '>@' + text + '</a>';
+      });
+    }
+      // <a _ngcontent-kqs-c6="" id="link" ng-reflect-router-link="/search/#" href="/search/%23">#</a>
+    document.getElementById('messageText').innerHTML = messageHTML;
+    console.log(document.getElementById('messageText'));
+    console.log(document.getElementById('link'));
+  }
+
+  onHashtagClick(event) {
+    this.router.navigateByUrl('/search');
+    console.log(event);
   }
 
 }
